6ea1271316a787e3fc4c0129c299c492c59c23eb
TAH4uk <pcs_zero@inbox.ru>
2022-08-30 14:11:13 +0300

    zadachi

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.Write("Введите число A: ");
+// double a = double.Parse(Console.ReadLine());
+
+// Console.Write("Введите положительное число B: ");
+// double b = double.Parse(Console.ReadLine());
+
+// if (b < 0)
+// {
+//     Console.Write("Вы ввели неправильное число! Введите положительное число B: ");
+//     b = double.Parse(Console.ReadLine());
+// }
+
+// double result = Math.Pow(a, b);
+
+// Console.WriteLine($"Ответ {result}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
