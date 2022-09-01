438e96580aa887682077b3dac23fd92243e317ca
TAH4uk <pcs_zero@inbox.ru>
2022-09-01 22:59:53 +0300

    zadachi

--- Program.cs
+++ Program.cs
@@ -1,43 +1,43 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// Console.Write("Введите число A: ");
-// double a = double.Parse(Console.ReadLine());
+Console.Write("Введите число A: ");
+double a = double.Parse(Console.ReadLine());
 
-// Console.Write("Введите положительное число B: ");
-// double b = double.Parse(Console.ReadLine());
+Console.Write("Введите положительное число B: ");
+double b = double.Parse(Console.ReadLine());
 
-// if (b < 0)
-// {
-//     Console.Write("Вы ввели неправильное число! Введите положительное число B: ");
-//     b = double.Parse(Console.ReadLine());
-// }
+if (b < 0)
+{
+    Console.Write("Вы ввели неправильное число! Введите положительное число B: ");
+    b = double.Parse(Console.ReadLine());
+}
 
-// double result = Math.Pow(a, b);
+double result = Math.Pow(a, b);
 
-// Console.WriteLine($"Ответ {result}");
+Console.WriteLine($"Ответ {result}");
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-// int NumSum(int number)
-// {
-//     int count = Convert.ToString(number).Length;
-//     int number2 = 0;
-//     int result = 0;
+int NumSum(int number)
+{
+    int count = Convert.ToString(number).Length;
+    int number2 = 0;
+    int result = 0;
 
-//     for (int i = 0; i < count; i++)
-//     {
-//         number2 = number - number % 10;
-//         result = result + (number - number2);
-//         number = number / 10;
-//     }
-//     return result;
-// }
+    for (int i = 0; i < count; i++)
+    {
+        number2 = number - number % 10;
+        result = result + (number - number2);
+        number = number / 10;
+    }
+    return result;
+}
 
-// int numsum = NumSum(number);
-// Console.WriteLine($"Сумма цифр в числе = {numsum}");
+int numsum = NumSum(number);
+Console.WriteLine($"Сумма цифр в числе = {numsum}");
 
 // Задача 29. Пользователь вводит число элементов в массиве и минимальное и максимальное значения элементов массива. 
 // Необходимо заполнить массив рандоными данными от минимального до максимального значения и вывести его.
